1151f857e055ad2558546fa353b5a473177a129c
Oriel-wq <150646686+Oriel-wq@users.noreply.github.com>
2026-09-25 17:40:50 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_ORIEL_TTPROJECT (
+  tt_um_ORIEL_TTPROJECT0110 (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
